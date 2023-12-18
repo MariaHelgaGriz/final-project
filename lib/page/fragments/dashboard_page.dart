1080e946fd95c70523be5aaec21bc093ff50bd3b
adhis <tiva.adhisti@ui.ac.id>
2023-12-18 18:20:59 +0700

    add dashbaord

---
+++ lib/page/fragments/dashboard_page.dart
@@ -0,0 +1,86 @@
+import 'package:booksforall/models/fragment.dart';
+import 'package:booksforall/pages/fragments/cart_page.dart';
+import 'package:booksforall/pages/fragments/home_page.dart';
+import 'package:booksforall/pages/fragments/qna_page.dart';
+import 'package:booksforall/pages/fragments/wishlist_page.dart';
+import 'package:flutter/material.dart';
+
+class DashboardPage extends StatefulWidget {
+  const DashboardPage({super.key});
+  static const route = '/dashboard';
+
+  @override
+  State<DashboardPage> createState() => _DashboardPageState();
+}
+
+class _DashboardPageState extends State<DashboardPage> {
+  final index = ValueNotifier(0);
+
+  final fragments = [
+    Fragment(
+      label: 'Home',
+      icon: 'assets/ic_home.png',
+      view: const HomePage(),
+    ),
+    Fragment(
+      label: 'Wishlist',
+      icon: 'assets/ic_wishlist.png',
+      view: const WishlistPage(),
+    ),
+    Fragment(
+      label: 'Cart',
+      icon: 'assets/ic_cart.png',
+      view: const CartPage(),
+    ),
+    Fragment(
+      label: 'QnA',
+      icon: 'assets/ic_qna.png',
+      view: const QnaPage(),
+    ),
+  ];
+
+  @override
+  void dispose() {
+    index.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListenableBuilder(
+        listenable: index,
+        builder: (context, child) {
+          return fragments[index.value].view;
+        },
+      ),
+      bottomNavigationBar: ListenableBuilder(
+        listenable: index,
+        builder: (context, child) {
+          return BottomNavigationBar(
+            currentIndex: index.value,
+            onTap: (n) => index.value = n,
+            selectedItemColor: Theme.of(context).primaryColor,
+            unselectedItemColor: Colors.grey,
+            selectedFontSize: 12,
+            unselectedFontSize: 12,
+            type: BottomNavigationBarType.fixed,
+            items: fragments.map((e) {
+              return BottomNavigationBarItem(
+                label: e.label,
+                icon: ImageIcon(
+                  AssetImage(e.icon),
+                  // color: Colors.grey,
+                ),
+                activeIcon: ImageIcon(
+                  AssetImage(e.icon),
+                  // color: Theme.of(context).primaryColor,
+                ),
+              );
+            }).toList(),
+          );
+        },
+      ),
+    );
+  }
+}
