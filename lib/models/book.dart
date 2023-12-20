a353eae2c1972f7a342ae48ccf32c550c494ea22
unknown <PascalPahlevi@gmail.com>
2023-12-20 23:13:57 +0700

    updated book.dart

--- lib/models/book.dart
+++ lib/models/book.dart
@@ -1,17 +1,42 @@
 class Book {
-   final String title;
-   final String cover;
-   final String description;
-   final double review;
-   final String author;
-   final double price;
+  final int id;
+  final String title;
+  final String author;
+  final String genre;
+  final String description;
+  final double rating;
+  final String url;
 
-   Book({
-     required this.title,
-     required this.cover,
-     required this.description,
-     required this.review,
-     required this.author,
-     required this.price,
-   });
- }
+  Book({
+    required this.id,
+    required this.title,
+    required this.url,
+    required this.description,
+    required this.genre,
+    required this.author,
+    required this.rating,
+  });
+
+  Map<String, dynamic> toJson() {
+    return <String, dynamic>{
+      'title': title,
+      'author': author,
+      'genre': genre,
+      'description': description,
+      'rating': rating.toDouble(),
+      'url': url,
+    };
+  }
+
+  factory Book.fromJson(Map<String, dynamic> json, int id) {
+    return Book(
+      id: id,
+      title: json['title'],
+      author: json['author'],
+      genre: json['genre'],
+      description: json['description'],
+      rating: json['rating'].toDouble(),
+      url: json['url'],
+    );
+  }
+}
