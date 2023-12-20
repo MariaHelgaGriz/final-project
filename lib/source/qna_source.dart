1f0c641328913e832ab610a6dfe46b6ccf0cfe1c
raisaafadilla <raisa.fadilla@ui.ac.id>
2023-12-20 23:19:17 +0700

    qna update

--- lib/source/qna_source.dart
+++ lib/source/qna_source.dart
@@ -1,79 +1,103 @@
 import 'dart:convert';
-import 'package:booksforall/models/question.dart';
+import 'package:booksforall/common/api.dart';
+import 'package:booksforall/models/qna.dart';
+import 'package:booksforall/models/user.dart';
 import 'package:d_method/d_method.dart';
 import 'package:d_session/d_session.dart';
 import 'package:dartz/dartz.dart';
 import 'package:http/http.dart' as http;
 
 class QnaSource {
-  static Future<Either<String, List<Question>>> all() async {
-    String url = 'http://127.0.0.1:8000/api/v1/get/get_question_answer/';
+  static Future<Either<String, List<QnA>>> all() async {
+    String url = '${Api.host}/api/v1/get/get_question_answer/';
     try {
-      String sessionId = await DSession.getCustom('session_id');
-      final response = await http.get(Uri.parse(url), headers: {
-        'cookie': sessionId,
-      });
+      final userMap = await DSession.getUser();
+      User user = User.fromJson(userMap);
+      String sessionId = user.sessionId;
+      final response = await http.get(
+        Uri.parse(url),
+        headers: {
+          'Cookie': 'sessionid=$sessionId',
+        },
+      );
       DMethod.logResponse(response);
 
       if (response.statusCode == 200) {
         final resBody = jsonDecode(response.body);
         List list = resBody['question_answer_list'];
         return Right(
-          list.map((e) {
-            int id = e['pk'];
-            final question = e['fields'];
-            return Question.fromJson(question, id);
-          }).toList(),
+          list.map((e) => QnA.fromJson(e)).toList(),
         );
       }
       return const Left('Server error');
     } catch (e) {
+      DMethod.log(e.toString());
       return const Left('Something went wrong');
     }
   }
 
   static Future<Either<String, String>> addQuestion(
-      String bookId, String question) async {
-    String url = 'http://127.0.0.1:8000/api/v1/add/create_question/';
+    int bookId,
+    String question,
+  ) async {
+    String url = '${Api.host}/api/v1/add/create_question/';
     try {
+      final userMap = await DSession.getUser();
+      User user = User.fromJson(userMap);
+      String sessionId = user.sessionId;
       final response = await http.post(
         Uri.parse(url),
+        headers: {
+          'Cookie': 'sessionid=$sessionId',
+        },
         body: {
-          'bookId': bookId,
+          'bookid': bookId.toString(),
           'question': question,
         },
       );
       DMethod.logResponse(response);
 
       if (response.statusCode == 201) {
-        return const Right('Success Add Question');
+        Map resBody = jsonDecode(response.body);
+        String message = resBody['message'];
+        return Right(message);
       }
       return const Left('Failed Add Question');
     } catch (e) {
+      DMethod.log(e.toString());
       return const Left('Something went wrong');
     }
   }
 
   static Future<Either<String, String>> addAnswer(
-    String questionId,
+    int questionId,
     String answer,
   ) async {
-    String url = 'http://127.0.0.1:8000/api/v1/add/create_answer/';
+    String url = '${Api.host}/api/v1/add/create_answer/';
     try {
+      final userMap = await DSession.getUser();
+      User user = User.fromJson(userMap);
+      String sessionId = user.sessionId;
       final response = await http.post(
         Uri.parse(url),
+        headers: {
+          'Cookie': 'sessionid=$sessionId',
+        },
         body: {
-          'questionId': questionId,
+          'questionid': questionId.toString(),
           'answer': answer,
         },
       );
       DMethod.logResponse(response);
 
       if (response.statusCode == 201) {
-        return const Right('Success Add Answer');
+        Map resBody = jsonDecode(response.body);
+        String message = resBody['message'];
+        return Right(message);
       }
-      return const Left('Failed Add Answer');
+      return const Left('Failed Add Question');
     } catch (e) {
+      DMethod.log(e.toString());
       return const Left('Something went wrong');
     }
   }
