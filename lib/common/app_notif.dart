604bec500e753fab1cc992d5042b205c3ef2be78
adhis <tiva.adhisti@ui.ac.id>
2023-12-20 22:25:01 +0700

    common

---
+++ lib/common/app_notif.dart
@@ -0,0 +1,35 @@
+import 'package:d_info/d_info.dart';
+import 'package:flutter/material.dart';
+
+class AppNotif {
+  static success(BuildContext context, String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+        backgroundColor: Colors.green,
+      ),
+    );
+  }
+
+  static failed(BuildContext context, String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+        backgroundColor: Colors.red,
+      ),
+    );
+  }
+
+  static invalid(BuildContext context, String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+        backgroundColor: Colors.red,
+      ),
+    );
+  }
+
+  static toastInvalid(BuildContext context, String message) {
+    DInfo.toastError(message);
+  }
+}
